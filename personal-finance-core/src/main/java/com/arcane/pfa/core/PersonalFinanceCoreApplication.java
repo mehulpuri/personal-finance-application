72cac73d7b940af19a884a70e311faf0ad1edb95
Ajit Kumar Gunturi <ajitg@vmware.com>
2024-02-13 20:38:45 +0530

    setup(spring-boot): initial-commit

---
+++ personal-finance-core/src/main/java/com/arcane/pfa/core/PersonalFinanceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.arcane.pfa.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceCoreApplication.class, args);
+	}
+
+}
---
+++ personal-finance-core/src/test/java/com/arcane/pfa/core/PersonalFinanceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcane.pfa.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
